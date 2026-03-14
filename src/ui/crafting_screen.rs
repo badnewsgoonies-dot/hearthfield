bdb07181ad00d8e626116c010d0aa4c3c4c06229
Claude Orchestrator <orchestrator@hearthfield.dev>
2026-03-14 16:04:19 +0000

    area3: task 1

--- src/ui/crafting_screen.rs
+++ src/ui/crafting_screen.rs
@@ -35,6 +35,18 @@ pub struct CraftingRecipeMaterials {
 #[derive(Component)]
 pub struct CraftingStatusText;
 
+#[derive(Component)]
+pub struct CraftingFeaturedIcon;
+
+#[derive(Component)]
+pub struct CraftingFeaturedName;
+
+#[derive(Component)]
+pub struct CraftingFeaturedDescription;
+
+#[derive(Component)]
+pub struct CraftingFeaturedMaterials;
+
 /// Tracks crafting UI state
 #[derive(Resource)]
 pub struct CraftingUiState {
@@ -92,8 +104,8 @@ pub fn spawn_crafting_screen(
             parent
                 .spawn((
                     Node {
-                        width: Val::Px(550.0),
-                        height: Val::Px(420.0),
+                        width: Val::Px(760.0),
+                        height: Val::Px(560.0),
                         flex_direction: FlexDirection::Column,
                         padding: UiRect::all(Val::Px(16.0)),
                         row_gap: Val::Px(6.0),
@@ -128,78 +140,153 @@ pub fn spawn_crafting_screen(
                         TextColor(Color::srgb(0.5, 0.9, 0.5)),
                     ));
 
-                    // Recipe list (up to 10 visible rows)
-                    for i in 0..10 {
-                        panel
-                            .spawn((
-                                CraftingRecipeRow { index: i },
+                    // Selected recipe feature card
+                    panel
+                        .spawn((
+                            Node {
+                                width: Val::Percent(100.0),
+                                min_height: Val::Px(170.0),
+                                flex_direction: FlexDirection::Row,
+                                column_gap: Val::Px(16.0),
+                                padding: UiRect::all(Val::Px(14.0)),
+                                border: UiRect::all(Val::Px(2.0)),
+                                margin: UiRect::bottom(Val::Px(6.0)),
+                                ..default()
+                            },
+                            BackgroundColor(Color::srgba(0.18, 0.13, 0.09, 0.92)),
+                            BorderColor(Color::srgb(0.56, 0.39, 0.19)),
+                        ))
+                        .with_children(|card| {
+                            card.spawn((
+                                CraftingFeaturedIcon,
                                 Node {
-                                    width: Val::Percent(100.0),
-                                    height: Val::Px(32.0),
-                                    flex_direction: FlexDirection::Row,
-                                    justify_content: JustifyContent::SpaceBetween,
-                                    align_items: AlignItems::Center,
-                                    padding: UiRect::horizontal(Val::Px(8.0)),
+                                    width: Val::Px(48.0),
+                                    height: Val::Px(48.0),
+                                    margin: UiRect::top(Val::Px(2.0)),
                                     ..default()
                                 },
-                                BackgroundColor(Color::srgba(0.2, 0.17, 0.14, 0.6)),
-                            ))
-                            .with_children(|row| {
-                                // Item icon
-                                let mut icon_cmd = row.spawn((
-                                    CraftingRecipeIcon { index: i },
+                            ));
+
+                            card.spawn((Node {
+                                flex_direction: FlexDirection::Column,
+                                row_gap: Val::Px(8.0),
+                                flex_grow: 1.0,
+                                ..default()
+                            },))
+                                .with_children(|details| {
+                                    details.spawn((
+                                        CraftingFeaturedName,
+                                        Text::new(""),
+                                        TextFont {
+                                            font: font_handle.0.clone(),
+                                            font_size: 20.0,
+                                            ..default()
+                                        },
+                                        TextColor(Color::srgb(1.0, 0.9, 0.72)),
+                                    ));
+                                    details.spawn((
+                                        CraftingFeaturedDescription,
+                                        Text::new(""),
+                                        TextFont {
+                                            font: font_handle.0.clone(),
+                                            font_size: 13.0,
+                                            ..default()
+                                        },
+                                        TextColor(Color::srgb(0.8, 0.76, 0.68)),
+                                    ));
+                                    details.spawn((
+                                        CraftingFeaturedMaterials,
+                                        Text::new(""),
+                                        TextFont {
+                                            font: font_handle.0.clone(),
+                                            font_size: 12.0,
+                                            ..default()
+                                        },
+                                        TextColor(Color::srgb(0.72, 0.68, 0.62)),
+                                    ));
+                                });
+                        });
+
+                    panel
+                        .spawn((Node {
+                            width: Val::Percent(100.0),
+                            flex_direction: FlexDirection::Column,
+                            row_gap: Val::Px(6.0),
+                            ..default()
+                        },))
+                        .with_children(|list| {
+                            // Recipe list (up to 6 visible rows)
+                            for i in 0..6 {
+                                list.spawn((
+                                    CraftingRecipeRow { index: i },
                                     Node {
-                                        width: Val::Px(24.0),
-                                        height: Val::Px(24.0),
-                                        margin: UiRect::right(Val::Px(6.0)),
+                                        width: Val::Percent(100.0),
+                                        height: Val::Px(40.0),
+                                        flex_direction: FlexDirection::Row,
+                                        justify_content: JustifyContent::SpaceBetween,
+                                        align_items: AlignItems::Center,
+                                        padding: UiRect::horizontal(Val::Px(8.0)),
                                         ..default()
                                     },
-                                ));
-                                if atlas_data.loaded {
-                                    if let Some(recipe_id) = visible.get(i) {
-                                        if let Some(recipe) =
-                                            recipe_registry.recipes.get(recipe_id.as_str())
-                                        {
-                                            if let Some(item_def) =
-                                                item_registry.get(&recipe.result)
+                                    BackgroundColor(Color::srgba(0.2, 0.17, 0.14, 0.6)),
+                                ))
+                                .with_children(|row| {
+                                    // Item icon
+                                    let mut icon_cmd = row.spawn((
+                                        CraftingRecipeIcon { index: i },
+                                        Node {
+                                            width: Val::Px(24.0),
+                                            height: Val::Px(24.0),
+                                            margin: UiRect::right(Val::Px(6.0)),
+                                            ..default()
+                                        },
+                                    ));
+                                    if atlas_data.loaded {
+                                        if let Some(recipe_id) = visible.get(i) {
+                                            if let Some(recipe) =
+                                                recipe_registry.recipes.get(recipe_id.as_str())
                                             {
-                                                icon_cmd.insert(ImageNode {
-                                                    image: atlas_data.image.clone(),
-                                                    texture_atlas: Some(TextureAtlas {
-                                                        layout: atlas_data.layout.clone(),
-                                                        index: item_icon_index(
-                                                            item_def.sprite_index,
-                                                        ),
-                                                    }),
-                                                    ..default()
-                                                });
+                                                if let Some(item_def) =
+                                                    item_registry.get(&recipe.result)
+                                                {
+                                                    icon_cmd.insert(ImageNode {
+                                                        image: atlas_data.image.clone(),
+                                                        texture_atlas: Some(TextureAtlas {
+                                                            layout: atlas_data.layout.clone(),
+                                                            index: item_icon_index(
+                                                                item_def.sprite_index,
+                                                            ),
+                                                        }),
+                                                        ..default()
+                                                    });
+                                                }
                                             }
                                         }
                                     }
-                                }
 
-                                row.spawn((
-                                    CraftingRecipeName { index: i },
-                                    Text::new(""),
-                                    TextFont {
-                                        font: font_handle.0.clone(),
-                                        font_size: 14.0,
-                                        ..default()
-                                    },
-                                    TextColor(Color::WHITE),
-                                ));
-                                row.spawn((
-                                    CraftingRecipeMaterials { index: i },
-                                    Text::new(""),
-                                    TextFont {
-                                        font: font_handle.0.clone(),
-                                        font_size: 11.0,
-                                        ..default()
-                                    },
-                                    TextColor(Color::srgb(0.6, 0.6, 0.6)),
-                                ));
-                            });
-                    }
+                                    row.spawn((
+                                        CraftingRecipeName { index: i },
+                                        Text::new(""),
+                                        TextFont {
+                                            font: font_handle.0.clone(),
+                                            font_size: 14.0,
+                                            ..default()
+                                        },
+                                        TextColor(Color::WHITE),
+                                    ));
+                                    row.spawn((
+                                        CraftingRecipeMaterials { index: i },
+                                        Text::new(""),
+                                        TextFont {
+                                            font: font_handle.0.clone(),
+                                            font_size: 11.0,
+                                            ..default()
+                                        },
+                                        TextColor(Color::srgb(0.6, 0.6, 0.6)),
+                                    ));
+                                });
+                            }
+                        });
 
                     // Hint
                     panel.spawn((
@@ -231,10 +318,12 @@ pub fn despawn_crafting_screen(
 
 #[allow(clippy::too_many_arguments, clippy::type_complexity)]
 pub fn update_crafting_display(
+    mut commands: Commands,
     ui_state: Option<Res<CraftingUiState>>,
     recipe_registry: Res<RecipeRegistry>,
     inventory: Res<Inventory>,
     item_registry: Res<ItemRegistry>,
+    atlas_data: Res<ItemAtlasData>,
     mut name_query: Query<
         (&CraftingRecipeName, &mut Text, &mut TextColor),
         Without<CraftingRecipeMaterials>,
@@ -252,12 +341,98 @@ pub fn update_crafting_display(
             Without<CraftingRecipeMaterials>,
         ),
     >,
+    mut featured_name_query: Query<
+        &mut Text,
+        (
+            With<CraftingFeaturedName>,
+            Without<CraftingFeaturedDescription>,
+            Without<CraftingFeaturedMaterials>,
+        ),
+    >,
+    mut featured_description_query: Query<
+        &mut Text,
+        (
+            With<CraftingFeaturedDescription>,
+            Without<CraftingFeaturedName>,
+            Without<CraftingFeaturedMaterials>,
+        ),
+    >,
+    mut featured_materials_query: Query<
+        &mut Text,
+        (
+            With<CraftingFeaturedMaterials>,
+            Without<CraftingFeaturedName>,
+            Without<CraftingFeaturedDescription>,
+        ),
+    >,
+    featured_icon_query: Query<Entity, With<CraftingFeaturedIcon>>,
 ) {
     let Some(ui_state) = ui_state else { return };
 
     // Status message
     for mut text in &mut status_query {
         **text = ui_state.status_message.clone();
+    }
+
+    let selected_recipe = ui_state
+        .visible_recipes
+        .get(ui_state.cursor)
+        .and_then(|recipe_id| recipe_registry.recipes.get(recipe_id));
+
+    let featured_description = selected_recipe
+        .and_then(|recipe| item_registry.get(&recipe.result))
+        .map(|item| item.description.clone())
+        .unwrap_or_default();
+    let featured_materials = selected_recipe
+        .map(|recipe| {
+            let mut parts = Vec::new();
+            for (item_id, qty) in &recipe.ingredients {
+                let name = item_registry
+                    .get(item_id)
+                    .map(|d| d.name.clone())
+                    .unwrap_or_else(|| item_id.clone());
+                let has = inventory.count(item_id) as u8;
+                parts.push(format!("{} ({}/{})", name, has, qty));
+            }
+            if parts.is_empty() {
+                "Materials: None".to_string()
+            } else {
+                format!("Materials: {}", parts.join(", "))
+            }
+        })
+        .unwrap_or_default();
+
+    for mut text in &mut featured_name_query {
+        **text = selected_recipe
+            .map(|recipe| recipe.name.clone())
+            .unwrap_or_default();
+    }
+
+    for mut text in &mut featured_description_query {
+        **text = featured_description.clone();
+    }
+
+    for mut text in &mut featured_materials_query {
+        **text = featured_materials.clone();
+    }
+
+    for entity in &featured_icon_query {
+        let mut entity_commands = commands.entity(entity);
+        entity_commands.remove::<ImageNode>();
+        if atlas_data.loaded {
+            if let Some(recipe) = selected_recipe {
+                if let Some(item_def) = item_registry.get(&recipe.result) {
+                    entity_commands.insert(ImageNode {
+                        image: atlas_data.image.clone(),
+                        texture_atlas: Some(TextureAtlas {
+                            layout: atlas_data.layout.clone(),
+                            index: item_icon_index(item_def.sprite_index),
+                        }),
+                        ..default()
+                    });
+                }
+            }
+        }
     }
 
     for (name_comp, mut text, mut color) in &mut name_query {
@@ -308,7 +483,7 @@ pub fn update_crafting_display(
     // Cursor highlight
     for (row, mut bg) in &mut row_query {
         if row.index == ui_state.cursor {
-            *bg = BackgroundColor(Color::srgba(0.35, 0.3, 0.2, 0.9));
+            *bg = BackgroundColor(Color::srgba(0.42, 0.26, 0.12, 0.96));
         } else {
             *bg = BackgroundColor(Color::srgba(0.2, 0.17, 0.14, 0.6));
         }
